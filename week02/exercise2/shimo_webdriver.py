5c21b0712bf4ce5f26d1b51a91fa7c024a9b2b91
zentiger <death.larson@163.com>
2020-08-03 10:31:04 +0800

    delete account

--- week02/exercise2/shimo_webdriver.py
+++ week02/exercise2/shimo_webdriver.py
@@ -6,7 +6,7 @@ try:
     browser.get("https://shimo.im")
     login_href = browser.find_element_by_xpath('//*[@id="homepage-header"]/nav/div[3]/a[2]/button') 
     login_href.click()
-    browser.find_element_by_xpath('//input[@name="mobileOrEmail"]').send_keys('120186766@qq.com')
+    browser.find_element_by_xpath('//input[@name="mobileOrEmail"]').send_keys('120xxxx@qq.com')
     browser.find_element_by_xpath('//input[@name="password"]').send_keys('TEST000')
     login_button = browser.find_element_by_xpath('//button')
     login_button.click()
